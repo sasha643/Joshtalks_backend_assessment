0820a1d3e5970e67450192bd45c24cd472b73c36
Saurabh Sharma <sasharma643@gmail.com>
2025-03-24 23:13:07 +0530

    Created models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    task_type = models.CharField(max_length=50, default='General')
+    assigned_users = models.ManyToManyField(User, related_name='tasks')
+
+    def __str__(self):
+        return self.name
